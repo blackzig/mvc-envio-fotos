a66c9afa11b483517eada574c5725b6c53afb125
michel adriano medeiros <michel adriano medeiros>
2017-07-22 12:23:35 -0300

    segunda fase completa

---
+++ core/Core.php
@@ -0,0 +1,33 @@
+<?php
+class Core{
+
+    public function run(){
+
+        $url ='/';
+        if(isset($_GET['url'])){
+            $url.=$_GET['url'];
+        }
+
+        if(!empty($url) && $url != '/'){
+            $url = explode('/', $url);
+            array_shift($url);
+
+            $currentController = $url[0].'Controller';
+            array_shift($url);
+            
+            if(isset($url[0]) && !empty($url[0])){
+                $currentAction = $url[0];
+            }else{
+                $currentAction = 'index';
+            }
+
+        }else{
+            $currentController = 'homeController';
+            $currentAction = 'index';
+        }
+
+        echo'currentController '.$currentController;
+        echo'<br/>currentController '.$currentAction;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 session_start();
 require 'config.php';
+
+spl_autoload_register(function($class){
+
+    $classe = $class.'.php';
+
+    if(file_exists('controllers/'.$classe)){
+        require 'controllers/'.$classe;
+    }else if(file_exists('models/'.$classe)){
+        require 'models/'.$classe;
+    }else if(file_exists('core/'.$classe)){
+        require 'core/'.$classe;
+    }
+});
+
+$core = new Core();
+$core->run();
