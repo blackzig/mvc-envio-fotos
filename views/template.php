45af94be4e692e80d746809902967bae6acb69f6
michel adriano medeiros <michel adriano medeiros>
2017-07-25 20:22:18 -0300

    Padrão mvc terminado

--- views/template.php
+++ views/template.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $viewName; ?></title>
+    <link rel="stylesheet" type="text/css" href="<?= BASE_URL; ?>assets/css/estilo.css" />
 </head>
 <body>
     <h1>TOPO</h1>
@@ -12,4 +13,6 @@
     <a href="<?= BASE_URL; ?>galeria">Galeria</a>
     <?php $this->loadViewInTemplate($viewName, $viewData); ?>
 </body>
+
+    <script src="<?= BASE_URL; ?>assets/js/meus-eventos.js"></script>
 </html>
