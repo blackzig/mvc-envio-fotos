c5d72f74a10eb4d4051037e7c442de660c977570
michel adriano medeiros <michel adriano medeiros>
2017-07-26 21:49:35 -0300

    adicionando página 404

---
+++ controllers/notFoundController.php
@@ -0,0 +1,8 @@
+<?php
+class notFoundController extends controller{
+    
+    public function index(){
+        $this->loadView('404',array());
+    }
+
+}
---
+++ views/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Alguma Coisa Está Errada</title>
+</head>
+<body>
+    <h1>Esta Página não Equesiste</h1>
+    <img src="https://s-media-cache-ak0.pinimg.com/originals/dc/6f/24/dc6f241de89bf985f0a0dde751491c4e.png" alt="página não encontrada" />
+</body>
+</html>
